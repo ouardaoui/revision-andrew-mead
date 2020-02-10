fce754d9591a44a7a89ccdaf24b6c1a51b82318d
ouardaoui <ayoubouardaoui3@gmail.com>
2020-02-10 19:54:54 +0100

    .

--- note.js
+++ note.js
@@ -1,4 +1,38 @@
-const ps = document.querySelectorAll('p')
-ps.forEach(function(p) {
-    p.textContent = "gfkrvvjfvl"
+// //const ps = document.querySelectorAll('p')
+// ps.forEach(function(p) {
+//     p.textContent = "gfkrvvjfvl"
+// })
+const notes = [{
+    title: "my next trip",
+    body: "I would like to go to spain"
+}, {
+    title: "Habbits to workk on",
+    boby: "Exercise,Eating a bit better"
+}, {
+    title: "modification",
+    boby: "Get a new seat"
+}]
+const filters = {
+    text: ''
+}
+const render = function(notes, filters) {
+    const filterednote = notes.filter(function(note) {
+        return note.title.toLowerCase().includes(filters.text.toLowerCase())
+    })
+    console.log(filterednote)
+}
+render(notes, filters)
+document.querySelector('#creat-note').addEventListener('click', function(e) {
+    e.target.textContent = "the note was created"
+})
+document.querySelector('#remove-all').addEventListener('click', function() {
+    document.querySelectorAll('.note').forEach(function(note) {
+        note.remove()
+    })
+})
+document.querySelector('#search-text').addEventListener('input', function(e) {
+    filters.text = e.target.value
+    render(notes, filters)
+
+
 })
--- todo.js
+++ todo.js
@@ -1,7 +1,34 @@
-const ps = document.querySelectorAll('p')
-ps.forEach(function(p) {
-    if (p.textContent.includes('the')) {
-        p.remove()
+const todos = [{
+    text: 'Ordre cat food',
+    completed: false
+}, {
+    text: 'clean kitchen',
+    completed: true
+}, {
+    text: 'Do work',
+    completed: false
+}, {
+    text: 'By food',
+    completed: true
+}, {
+    text: 'Exercise',
+    completed: true
+}]
 
-    }
+const a = todos.filter(function(todos) {
+    return !todos.completed
+})
+const summary = document.createElement('h2')
+summary.textContent = `you have ${a.length} to do left`
+document.querySelector('body').appendChild(summary)
+todos.forEach(function(todo) {
+    const p = document.createElement('p')
+    p.textContent = todo.text
+    document.querySelector('body').appendChild(p)
+})
+document.querySelector('#add-todo').addEventListener('click', function(e) {
+    console.log("add a new to do...")
+})
+document.querySelector('#new-todo').addEventListener('input', function(e) {
+    console.log(e.target.value)
 })
